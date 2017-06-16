b94ea70cb11d513208ca6a5d8748172fc01d2f9f
Ryan Abbott <ryan@element84.com>
2017-06-16 15:00:55 -0400

    MMT-990: Adds form layout json

--- app/controllers/variables_controller.rb
+++ app/controllers/variables_controller.rb
@@ -1,11 +1,26 @@
+# :nodoc:
 class VariablesController < ApplicationController
   def index
     set_schema
+    set_form
+    set_object
   end
 
   private
 
   def set_schema
     @schema = JSON.parse(File.read(File.join(Rails.root, 'lib', 'assets', 'schemas', 'umm-var-json-schema.json')))
+  end
+
+  def set_form
+    @form = JSON.parse(File.read(File.join(Rails.root, 'lib', 'assets', 'schemas', 'umm-var-form.json')))
+  end
+
+  def set_object
+    @object = {
+      'Name'                 => 'Sollicitudin Vestibulum',
+      'DimensionsName'       => 'Lorem Cras Pellentesque Dolor Elit',
+      'FillValueDescription' => 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.'
+    }
   end
 end
--- app/helpers/umm_helper.rb
+++ app/helpers/umm_helper.rb
@@ -1,18 +1,93 @@
 # :nodoc:
 module UmmHelper
-  def property_classes(property)
+  def element_classes(property)
+    # Default classes
     classes = ['full-width']
 
+    # Add textcounter to the UI if the element has a maxLength
     classes.append('textcounter') if property.key?('maxLength')
 
     classes.join(' ')
   end
 
-  def validation_properties(property_name, property, required_fields: [])
-    validation_properties = property.select { |key| %w(minLength maxLength).include?(key) }
+  def validation_properties(element, schema)
+    # jQuery Validate can use html elements for validation so we'll set those elements
+    # here instead of having to define these attributes on a one off basis in javascript
+    validation_properties = element.select { |key| %w(minLength maxLength).include?(key) }
 
-    validation_properties['required'] = required_fields.include?(property_name)
+    # JSON Schema provides the required fields in a separate array so we have to look this up
+    validation_properties['required'] = schema_required_fields(schema).include?(element['key'])
 
     validation_properties
+  end
+
+  def element_properties(element, schema)
+    { class: element_classes(element) }.merge(validation_properties(element, schema))
+  end
+
+  def render_form_element(element, schema, object)
+    if element['type'] == 'section'
+      render_section(element, schema, object)
+    else
+      send('render_markup', element.fetch('type', 'text'), hydrate_schema_property(schema, element['key']), schema, object)
+    end
+  end
+
+  def render_markup(type, element, schema, object)
+    capture do
+      # Label for the form field
+      concat render_label(element, schema)
+
+      # Help Icon and Modal
+      concat mmt_help_icon({ help: "properties/#{element['key']}", title: 'element' })
+
+      # Render the field
+      concat send("render_#{type}", element, schema, object)
+    end
+  end
+
+  def render_section(element, schema, object)
+    content_tag(:section, class: element['htmlClass']) do
+      # Display a title for the section if its provided
+      concat content_tag(:h4, element['title'], class: 'space-bot') if element.key?('title')
+
+      # Display a description of the section if its provided
+      concat content_tag(:p, element['description'], class: 'form-description space-bot') if element.key?('description')
+
+      # Continue rendering fields that appear in this section
+      element.fetch('items', []).each do |child_element|
+        concat render_form_element(child_element, schema, object)
+      end
+    end
+  end
+
+  def render_textarea(element, schema, object)
+    text_area_tag(element['key'], object[element['key']], element_properties(element, schema))
+  end
+
+  def render_text(element, schema, object)
+    text_field_tag(element['key'], object[element['key']], element_properties(element, schema))
+  end
+
+  def render_array(element, schema, object)
+    select_tag(element['key'], options_for_select(element['enum'], object[element['key']]), element_properties(element, schema))
+  end
+
+  def render_label(element, schema)
+    label_tag(element['key'].underscore, element['key'].titleize, class: ('eui-required-o' if schema_required_fields(schema).include?(element['key'])))
+  end
+
+  def schema_required_fields(schema)
+    schema.fetch('required', [])
+  end
+
+  def hydrate_schema_property(schema, key)
+    # Retreive the requested key from the schema
+    property = schema['properties'].fetch(key, {})
+
+    # Set the 'key' attribute within the property has so that we have reference to it
+    property['key'] = key
+
+    property
   end
 end
