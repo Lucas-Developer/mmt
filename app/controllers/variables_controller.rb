b1083aa3e843da366ddb791e4079b4bed28b6a5f
Ryan Abbott <ryan@element84.com>
2017-06-21 13:43:22 -0400

    MMT-900: Working recursive schema parsing?

--- app/controllers/variables_controller.rb
+++ app/controllers/variables_controller.rb
@@ -31,71 +31,31 @@ class VariablesController < ApplicationController
     # Load the requested schema file from the schema directory
     schema_file = JSON.parse(File.read(File.join(Rails.root, 'lib', 'assets', 'schemas', filename)))
 
-    fetch_references(schema_file)
+    fetch_references(schema_file, schema_file)
   end
 
-  def fetch_references(schema_file)
-    puts schema_file.to_s.scan(/(?=\$ref)/).count
+  def fetch_references(schema_file, property)
+    property.each do |_key, element|
+      next unless element.is_a?(Hash)
 
-    referenced_properties = schema_file.fetch('properties', {}).select { |key, property| property.key?('$ref') }
+      if element.key?('$ref')
+        file, path = element['$ref'].split('#')
 
-    referenced_properties.each do |key, property|
-      referenced_property = fetch_property_reference(property, schema_file)
+        referenced_property = if file.blank?
+                                schema_file['definitions'][path.split('/').last]
+                              else
+                                # If the reference in a different file -- parse it
+                                referenced_schema = parse_schema(file)
+                                referenced_schema['definitions'][path.split('/').last]
+                              end
 
-      # Merge the property into the reference incase we've overridden anything
-      schema_file['properties'][key] = referenced_property.merge(schema_file['properties'][key])
-      schema_file['properties'][key].delete('$ref')
-
-      if referenced_property.fetch('properties', {}).key?('$ref') || referenced_property.fetch('properties', {}).map { |key, property| property.fetch('items', {}).key?('$ref') }.any?
-        # fetch_references(schema_file)
+        element.merge!(referenced_property)
+        element.delete('$ref')
       end
-    end
 
-    referenced_items = schema_file.fetch('properties', {}).select { |key, property| property.fetch('items', {}).key?('$ref') }
-    referenced_items.each do |key, property|
-      referenced_property = fetch_items_reference(property, schema_file)
-
-      # Merge the property into the reference incase we've overridden anything
-      schema_file['properties'][key]['items'] = referenced_property.merge(schema_file['properties'][key]['items'])
-      schema_file['properties'][key]['items'].delete('$ref')
-
-      if referenced_property.fetch('properties', {}).key?('$ref') || referenced_property.fetch('properties', {}).map { |key, property| property.fetch('items', {}).key?('$ref') }.any?
-        # fetch_references(schema_file)
-      end
-    end
+      fetch_references(schema_file, element)
+    end  
 
     schema_file
-  end
-
-  def fetch_property_reference(property, current_schema)
-    file, path = property['$ref'].split('#')
-
-    referenced_property = unless file.blank?
-      # If the reference in a different file -- parse it
-      # referenced_schema = JSON.parse(File.read(File.join(Rails.root, 'lib', 'assets', 'schemas', file)))
-      referenced_schema = parse_schema(file)
-      referenced_schema['definitions'][path.split('/').last]
-    else
-      current_schema['definitions'][path.split('/').last]
-    end
-
-    fetch_items_reference(referenced_property, current_schema) if property.fetch('items', {}).key?('$ref')
-
-    referenced_property
-  end
-
-  def fetch_items_reference(property, current_schema)
-    file, path = property['items']['$ref'].split('#')
-
-    referenced_items = unless file.blank?
-      # If the reference in a different file -- parse it
-      # referenced_schema = JSON.parse(File.read(File.join(Rails.root, 'lib', 'assets', 'schemas', file)))
-      referenced_schema = parse_schema(file)
-      referenced_schema['definitions'][path.split('/').last]
-    else
-      current_schema['definitions'][path.split('/').last]
-    end
-
-    referenced_items
   end
 end
